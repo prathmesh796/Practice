7a9a70b7a0ab329b39da81ea7f9e57a1dc4ec002
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2024-07-31 22:26:13 +0530

    closures

--- JavaScript/closures.js
+++ JavaScript/closures.js
@@ -1,3 +1,4 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 // A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
 
 let msg = "hello 1"
