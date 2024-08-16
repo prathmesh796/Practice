14b48cd48d56817a63dfb6d005158e83ea415fdd
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2024-08-16 23:08:07 +0530

    responses and routes in express js

--- JavaScript/Express.js/express_main.js
+++ JavaScript/Express.js/express_main.js
@@ -1,11 +1,14 @@
 //ref - https://expressjs.com/
 
 const express = require('express')
+const blogs = require('./routes/blog.js')
+
 const app = express()
 const port = 3000
 
 //this is used to serve static file in the website(static files means those file which are made public avaliable to the end user)
 app.use(express.static('public'))
+app.use('/blog', blogs)
 
 //syntax - app.get(path, handler)
 app.get('/', (req, res) => {
@@ -36,6 +39,43 @@ app.get('/contact/:first', (req, res) => {
 app.get('/about/:one/:two', (req, res) => {
     res.send(`this is a contact page with ${req.params.one} and ${req.params.two} subpage`)
 })
+
+
+//POST req
+app.post('/', (req, res) => {
+    console.log("this is a post req")
+    res.send('Hello World!')
+})
+
+//PUT req
+app.put('/', (req, res) => {
+    console.log("this is a put req")
+    res.send('Hello World!')
+})
+
+/*
+we can also use the chaining syntax to write all the GET, POST, PUT, etc. req with a single app as follows:
+
+app.get('/', (req, res) => {
+    res.send('Hello World!, hi ')
+}).post('/', (req, res) => {
+    console.log("this is a post req")
+    res.send('Hello World!')
+}).put('/', (req, res) => {
+    console.log("this is a put req")
+    res.send('Hello World!')
+})
+
+this is called as Chaining of Request
+this will also work same
+*/
+
+//rendering html files
+app.get('/home', (req, res) => {
+    console.log("this is rendering of home.html")
+    res.sendFile('templates/home.html', {root: __dirname})
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
