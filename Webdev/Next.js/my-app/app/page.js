c5894885f68aa863f73510bca2848b8af2871f62
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2024-09-07 23:10:38 +0530

    getting server side and client side of next.js

--- Webdev/Next.js/my-app/app/page.js
+++ Webdev/Next.js/my-app/app/page.js
@@ -1,8 +1,10 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <div>
         hi, this is my first next.js app
       </div>
---
+++ Webdev/Next.js/my-app/components/Navbar.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      i am a navbar
+    </div>
+  )
+}
+
+export default Navbar
